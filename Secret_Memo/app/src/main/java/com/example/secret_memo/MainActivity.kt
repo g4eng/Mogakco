cb0991a2bf75b6760583b123472fd9f8f0b91217
Yang Kyeongmo <gaeng1107@gmail.com>
2021-11-24 23:51:48 +0900

    211124

--- Secret_Memo/app/src/main/java/com/example/secret_memo/MainActivity.kt
+++ Secret_Memo/app/src/main/java/com/example/secret_memo/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.secret_memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // EditText 컴포넌트를 찾아서 editText 변수에 저장
+        val editText = findViewById<EditText>(R.id.editTextNumberPassword)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            // editText에서 사용자가 입력한 텍스트 가져오기
+            val passwordText = editText.text.toString()
+
+            val password = "1234"
+            if (passwordText == password) {
+                Log.d("Password", "통과")
+            } else {
+                Log.d("Password", "틀렸어요.")
+            }
+        }
     }
 }
